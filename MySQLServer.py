f42d8a8c028e1e229c975d6890da184520889745
chornate <155074527+chornate@users.noreply.github.com>
2024-10-25 22:34:31 +0300

    Create MySQLServer.py

---
+++ MySQLServer.py
@@ -0,0 +1,16 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="blahblahblah",
+)
+
+mycursor = mydb.cursor()
+cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+            print("Database 'alx_book_store' created successfully!")
+
+    except Error as e:
+        print(f"Error: {e}")
+mycursor.close()
+mydb.close()
